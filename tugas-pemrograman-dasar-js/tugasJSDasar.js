da1d6d2d010e6d9104f411536a7dad367481578d
fadhlysani <33391379+fadhlysani@users.noreply.github.com>
2025-06-17 16:58:51 +0700

    Create tugasJSDasar.js

---
+++ tugas-pemrograman-dasar-js/tugasJSDasar.js
@@ -0,0 +1,13 @@
+var perkenalan = 'Nama saya adalah';
+var nama = "Muhammad Fadhly Sani"; // isi dengan nama kalian
+var kata1 = "saya";
+var kata2 = "berharap";
+var kata3 = "untuk";
+var kata4 = "menjadi";
+var kata5 = "seorang";
+var kata6 = "software";
+var kata7 = "engineer";
+
+var combine = perkenalan + " " + nama + " " + kata1 + " " + kata2 + " " + kata3 + " " + kata4 + " " + kata5 + " " + kata6 + " " + kata7;
+
+console.log(combine);
